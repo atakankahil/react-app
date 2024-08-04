690810dd47755f93bf63085863ebf66f5bf61f99
Atakan <atakankahil2811@gmail.com>
2024-08-04 20:59:28 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Books from './Books';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Books />
+            </header>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Books.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+
+const Books = () => {
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        fetch('/books')
+            .then(response => response.json())
+            .then(data => setBooks(data))
+            .catch(error => console.error('Error fetching books:', error));
+    }, []);
+
+    return (
+        <div>
+            <h1>Books</h1>
+            <ul>
+                {books.map(book => (
+                    <li key={book.id}>{book.title} by {book.author}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Books;
